6bed5a80dcb470c9e44a389e6d270bb527a823dc
Jonathan Krän <jonathan.kran1@gmail.com>
2026-09-09 09:01:25 +0200

    feat: add target-specific build outputs

--- build.bat
+++ build.bat
@@ -6,6 +6,7 @@ setlocal enabledelayedexpansion
 for /f %%a in ('git rev-parse --short HEAD 2^>NUL') do set commit_hash=%%a
 for /f %%d in ('powershell -command "[DateTime]::UtcNow.ToString('yyyy-MM-dd')"') do set today=%%d
 set version=dev-%today%-%commit_hash%
+set build_dir=build\windows
 
 if "%1" == "test" (
     odin test tests -collection:src=src -debug -define:ODIN_TEST_THREADS=1 -define:ODIN_TEST_TRACK_MEMORY=false -extra-linker-flags:"/STACK:4000000,2000000"
@@ -14,7 +15,9 @@ if "%1" == "test" (
 ) else if "%1" == "build_test" (
 	odin build tests -build-mode:test -collection:src=src -define:ODIN_TEST_THREADS=1 -define:ODIN_TEST_TRACK_MEMORY=false -extra-linker-flags:"/STACK:4000000,2000000" %2
 ) else if "%1" == "debug" (
-    odin build src\ -show-timings  -microarch:native  -collection:src=src  -out:ols.exe -o:minimal  -no-bounds-check -use-separate-modules -debug  -extra-linker-flags:"/STACK:4000000,2000000" -define:VERSION=%version%-debug
+    if not exist "%build_dir%" mkdir "%build_dir%"
+    odin build src\ -show-timings  -microarch:native  -collection:src=src  -out:%build_dir%\ols.exe -o:minimal  -no-bounds-check -use-separate-modules -debug  -extra-linker-flags:"/STACK:4000000,2000000" -define:VERSION=%version%-debug
 ) else (
-    odin build src\ -show-timings -microarch:native -collection:src=src -out:ols.exe -o:speed  -no-bounds-check  -extra-linker-flags:"/STACK:4000000,2000000" -define:VERSION=%version%
+    if not exist "%build_dir%" mkdir "%build_dir%"
+    odin build src\ -show-timings -microarch:native -collection:src=src -out:%build_dir%\ols.exe -o:speed  -no-bounds-check  -extra-linker-flags:"/STACK:4000000,2000000" -define:VERSION=%version%
 )
