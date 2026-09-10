76372c852546380f7ce6eaabfe4cb4a2849d5bdd
Jonathan Krän <jonathan.kran1@gmail.com>
2026-09-10 22:30:17 +0200

    fix: update output path for odinfmt executable

--- odinfmt.bat
+++ odinfmt.bat
@@ -1,9 +1,12 @@
 @echo off
 
 setlocal enabledelayedexpansion
+set build_dir=build\windows
+
+if not exist "%build_dir%" mkdir "%build_dir%"
 
 if "%1" == "debug" (
-	odin build tools/odinfmt/main.odin -file -show-timings  -collection:src=src -out:odinfmt.exe -o:none
+	odin build tools/odinfmt/main.odin -file -show-timings -collection:src=src -out:%build_dir%\odinfmt.exe -o:none
 ) else (
-	odin build tools/odinfmt/main.odin -file -show-timings  -collection:src=src -out:odinfmt.exe -o:speed
+	odin build tools/odinfmt/main.odin -file -show-timings -collection:src=src -out:%build_dir%\odinfmt.exe -o:speed
 )
